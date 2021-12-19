ae0384ce4da742107ac818d0cc0b5f82717f78a1
Weidi Qi <55336568+qiweidi@users.noreply.github.com>
2021-12-19 02:14:22 -0800

    Update i_CENTER.ino

--- i_CENTER.ino
+++ i_CENTER.ino
@@ -1,14 +1,25 @@
 // Function to write all servos to its neutral position
 void centerPosition()  
 {
-  leftAnkle.write(leftAnklePos);
-  rightAnkle.write(rightAnklePos);  
-  leftHip.write(leftHipPos);  
-  rightHip.write(rightHipPos); 
-  leftShoulder.write(leftShoulderPos); 
-  rightShoulder.write(rightShoulderPos); 
+  leftAnkle.write(100);
+  rightAnkle.write(120);  
+  leftHip.write(95);  
+  rightHip.write(120); 
+  leftShoulder.write(40); 
+  rightShoulder.write(150); 
   delay(1000);
 }//end centerPosition();
+
+//void centerPosition()  
+//{
+  //leftAnkle.write(leftAnklePos);
+  //rightAnkle.write(rightAnklePos);  
+  //leftHip.write(leftHipPos);  
+  //rightHip.write(rightHipPos); 
+  //leftShoulder.write(leftShoulderPos); 
+  //rightShoulder.write(rightShoulderPos); 
+  //delay(1000);
+//}//end centerPosition();
 
 
 //Function to write only HIP and ANKLE servos to center position
