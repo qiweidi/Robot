86fdf043d763b3ec376c41e085ad8290321a5dec
Weidi Qi <55336568+qiweidi@users.noreply.github.com>
2021-12-19 02:11:09 -0800

    Update c_LOOP.ino

--- c_LOOP.ino
+++ c_LOOP.ino
@@ -1,7 +1,27 @@
-
-   
 /**LOOP*******************************************************************************/
 
 void loop() 
-{//empty 
-}
+{ 
+  for(int i = 0; i < 5 ; i++)
+  {ledBlink();}
+  //delay(1000);
+  
+  
+  //Move Forward
+  for( int i = 0; i<20 ; i++ ) 
+ {forward();
+ 
+  }
+
+  for( int i = 0; i<10 ; i++ ) 
+ {handOut();
+  }
+ delay(1000);
+
+ for( int i = 0; i<10 ; i++ ) 
+ {DANCE();
+ }
+delay(1000);
+ // back to neutral 
+  centerPosition();
+  }
