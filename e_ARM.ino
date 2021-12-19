db35cf48f15ee89790d5678bcb69024e426d83ba
Weidi Qi <55336568+qiweidi@users.noreply.github.com>
2021-12-19 02:12:18 -0800

    Update e_ARM.ino

--- e_ARM.ino
+++ e_ARM.ino
@@ -3,9 +3,10 @@
 //Moves hands to outward position from NEUTRAL position
 void handOut ()               
 {
- for (int i = 0; i < 90 ; i+= 5 )
+ for (int i = 0; i < 180 ; i+= 5 )
  {leftShoulder.write(leftShoulderPos + i );
- rightShoulder.write(rightShoulderPos - i);}
+ //rightShoulder.write(rightShoulderPos - i);
+ }
 }//end handOut()
 
 /********************************************************/
