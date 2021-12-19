a044b19ca52c0b9ca06a52ec20d1fe0ed896f6bf
Weidi Qi <55336568+qiweidi@users.noreply.github.com>
2021-12-19 02:12:57 -0800

    Update f_ANKLE.ino

--- f_ANKLE.ino
+++ f_ANKLE.ino
@@ -8,7 +8,6 @@ void leftAnkleOut ()
     delay(forDelay);} 
 }
 
-
 void rightAnkleOut()
 {for (int i = 0; i < 10 ; i++ )
     {rightAnkle.write(rightAnklePos-i);   //right ankle out 
@@ -29,17 +28,38 @@ void ankleInLoop()
 {
   for (int i = 0; i <25 ; i++)
 {
-  leftAnkle.write((leftAnklePos -15)+i);   
-  rightAnkle.write((rightAnklePos-10)+i);
+  leftAnkle.write((leftAnklePos -10)+i);   
+  rightAnkle.write((rightAnklePos-1)+i);
   delay(forDelay); }
 }
+
 
 void ankleOutLoop()
 {
  for (int i = 0; i <25 ; i++ )
 {
-  rightAnkle.write((rightAnklePos+15)-i);   //right foot 
+  rightAnkle.write((rightAnklePos+10)-i);   //right foot 
   if (i <= 20)
    {leftAnkle.write((leftAnklePos+10)-i);}
    delay(forDelay);  } 
 }
+
+
+///Dancing movemeny testing
+ void ankleDanceLoop()
+  {
+  for (int i = 0; i <25 ; i++)
+  {
+  leftAnkle.write((leftAnklePos-30)-i);   
+  rightAnkle.write((rightAnklePos-30)-i);
+  delay(forDelay); }
+  }
+
+void ankleDanceOutLoop()
+  {
+  for (int i = 0; i <25 ; i++)
+  {
+  leftAnkle.write((leftAnklePos+30)+i);   
+  rightAnkle.write((rightAnklePos+30)+i);
+  delay(forDelay); }
+  }
